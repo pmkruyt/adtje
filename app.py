1d0cd1e8ef6b801b1b7631edfa91dbe3a59bf991
pmkruyt <102258821+pmkruyt@users.noreply.github.com>
2022-12-18 13:28:51 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ import pandas as pd
 app = Flask(__name__,template_folder='')
 
 
-df = pd.read_csv("data.csv")
-df=df[['web_name','now_cost','first_name']]
+#df = pd.read_csv("data.csv")
+#df=df[['web_name','now_cost','first_name']]
 
 @app.route('/')
 def home():
-   
-   return render_template("index.html", data=df.to_html())
+   dataframes = []
+   for i in range(1, 3):
+       df = pd.read_csv(f"data{i}.csv")
+       df2=df.to_html()
+       dataframes.append(df.to_html())
+   return render_template("index.html", data=dataframes)
+
 
 if __name__ == '__main__':
    app.run()
